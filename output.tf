a654fd71dc138924e9a2c41327aeddd6d8ea8f09
GonGarciaFontenla <ggarciafontenla@frba.utn.edu.ar>
2026-08-13 17:16:05 -0300

    Add instance_public_ip output for EC2 game server

--- output.tf
+++ output.tf
@@ -7,3 +7,8 @@ output "game_url" {
   description = "URL completa del juego"
   value       = "http://${aws_route53_record.game_server.name}"
 }
+
+output "instance_public_ip" {
+  description = "IP pública de la instancia EC2"
+  value       = aws_instance.game_server.public_ip
+}
